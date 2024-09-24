7d5aa5b702591c4cf2336ee61c344166f263b5d7
Kazi Yusuf <yfaka001@gmail.com>
2024-09-24 22:02:28 +0600

    Buttons for toggling between donation page and history page is now working fine!

--- src/app.js
+++ src/app.js
@@ -36,7 +36,26 @@ function donateNow(donationField, totalDonated) {
         // console.log(donatedNoakhali, donatedFeni, donatedForInjured); // Worked fine.
 };
 
-donateNow();
+// Page routing function - Working well
+function sectionToggle(sectFrom, sectTo) {
+    sectFrom.classList.replace('flex', 'hidden');
+    sectTo.classList.replace('hidden', 'flex');
+};
 
+// Connection between functions and buttons
+
+// History page button
+historyPageButton.addEventListener("click", function () {
+    sectionToggle(donationSection, historySection);
+    historyPageButton.classList.remove('btn-outline');
+    donationPageButton.classList.add('btn-outline');
+});
+
+// Donation page button
+donationPageButton.addEventListener("click", function() {
+    sectionToggle(historySection, donationSection);
+    donationPageButton.classList.remove('btn-outline');
+    historyPageButton.classList.add('btn-outline');
+});
 
 
