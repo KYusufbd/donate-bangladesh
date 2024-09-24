b0b39444ce2995a49858aac8a6014949160b8593
Kazi Yusuf <yfaka001@gmail.com>
2024-09-24 21:22:02 +0600

    Declared more variables

--- src/app.js
+++ src/app.js
@@ -16,17 +16,24 @@ const donateInjured = document.getElementById('donate-injured');
 const historyPageButton = document.getElementById('history-page');
 const donationPageButton = document.getElementById('donation-page');
 
+// Sections
+const donationSection = document.getElementById('donation-section');
+const historySection = document.getElementById('history-section');
+
 // Total donated
-const donatedNoakhali = document.getElementById('donated-noakhali').innerText;
-const donatedFeni = document.getElementById('donated-feni').innerText;
-const donatedForInjured = document.getElementById('donated-for-injured').innerText;
+const donatedNoakhali = parseFloat(document.getElementById('donated-noakhali').innerText);
+const donatedFeni = parseFloat(document.getElementById('donated-feni').innerText);
+const donatedForInjured = parseFloat(document.getElementById('donated-for-injured').innerText);
 
 // Donate Now function
 function donateNow(donationField, totalDonated) {
     const currentBalance = parseFloat(document.getElementById('current-balance').innerText);
-    // console.log(currentBalance.toFixed(2)); // Ok!
-    // console.log(donationForNoaKhali, donationForFeni, aidForInjured); // Worked well!!
-    // console.log(donateNoakhali, donateFeni, donateInjured); // Worked fine.
+
+    // Log to console for testing purpose.
+        // console.log(currentBalance.toFixed(2)); // Ok!
+        // console.log(donationForNoaKhali, donationForFeni, aidForInjured); // Worked well!!
+        // console.log(donateNoakhali, donateFeni, donateInjured); // Worked fine.
+        // console.log(donatedNoakhali, donatedFeni, donatedForInjured); // Worked fine.
 };
 
 donateNow();
