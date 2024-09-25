8f47636e262d4991d166f5cec46134c6054e28d0
Kazi Yusuf <yfaka001@gmail.com>
2024-09-25 11:12:29 +0600

    Calculation functions for all donations are working fine alhamdulillah. Now time to build history section

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
-// Function to get input amount
-function inputAmount(id) {
-    return document.getElementById(id).value;
-};
-
 // Input Amounts
-const donationForNoaKhali = inputAmount('donation-for-noakhali');
-const donationForFeni = inputAmount('donation-for-feni');
-const aidForInjured = inputAmount('aid-for-injured');
+
+const donationForNoaKhali = document.getElementById('donation-for-noakhali');
+const donationForFeni = document.getElementById('donation-for-feni');
+const aidForInjured = document.getElementById('aid-for-injured');
 
 // Buttons
 const donateNoakhali = document.getElementById('donate-noakhali');
@@ -21,19 +17,33 @@ const donationSection = document.getElementById('donation-section');
 const historySection = document.getElementById('history-section');
 
 // Total donated
-const donatedNoakhali = parseFloat(document.getElementById('donated-noakhali').innerText);
-const donatedFeni = parseFloat(document.getElementById('donated-feni').innerText);
-const donatedForInjured = parseFloat(document.getElementById('donated-for-injured').innerText);
+const donatedNoakhali = document.getElementById('donated-noakhali');
+const donatedFeni = document.getElementById('donated-feni');
+const donatedForInjured = document.getElementById('donated-for-injured');
 
 // Donate Now function
-function donateNow(donationField, totalDonated) {
-    const currentBalance = parseFloat(document.getElementById('current-balance').innerText);
+function donateNow(input, totalDonated) {
+    const balanceDisplay = document.getElementById('current-balance');
+    const balanceAmount = parseFloat(document.getElementById('current-balance').innerText);
+    const inputAmount = input.value;
+    
+    if (!typeof(inputAmount) == "number" || inputAmount < 1) {
+        alert('Please input a valid number!');
+    }
 
-    // Log to console for testing purpose.
-        // console.log(currentBalance.toFixed(2)); // Ok!
-        // console.log(donationForNoaKhali, donationForFeni, aidForInjured); // Worked well!!
-        // console.log(donateNoakhali, donateFeni, donateInjured); // Worked fine.
-        // console.log(donatedNoakhali, donatedFeni, donatedForInjured); // Worked fine.
+    else if (inputAmount > balanceAmount) {
+        alert('You don\'t have sufficient balance!');
+    }
+
+    else {
+        const currentBalance = balanceAmount - inputAmount;
+        balanceDisplay.innerText = `${currentBalance.toFixed(2)} BDT`;
+
+        const donatedAmount = parseFloat(totalDonated.innerText);
+        const newAmount = (parseFloat(donatedAmount) + parseFloat(inputAmount));
+        totalDonated.innerText = `${newAmount.toFixed(2)} BDT`;
+        input.value = '';
+    }
 };
 
 // Page routing function - Working well
@@ -58,4 +68,24 @@ donationPageButton.addEventListener("click", function() {
     historyPageButton.classList.add('btn-outline');
 });
 
+// Donate for Noakhali button
+donateNoakhali.addEventListener('click', function(event) {
+    event.preventDefault();
+    donateNow(donationForNoaKhali, donatedNoakhali);
+});
 
+// Donate for Feni button
+donateFeni.addEventListener('click', function(event) {
+    event.preventDefault();
+    donateNow(donationForFeni, donatedFeni);
+});
+
+// Donate for injured in quota movement button
+donateInjured.addEventListener('click', function(event) {
+    event.preventDefault();
+    donateNow(aidForInjured, donatedForInjured);
+});
+
+
+// To start Tailwind CLI build process: 
+// npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
